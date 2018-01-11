7f46069311c572e017510444e1b86cdb8bdb0bf1
Shaurya Sinha <ShauryaSinha@ASHUTOSHs-MacBook-Pro.local>
2018-01-11 12:12:09 -0500

    First Commit

---
+++ Tumblr-Feed/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  Tumblr-Feed
+//
+//  Created by Shaurya Sinha on 10/01/18.
+//  Copyright © 2018 Shaurya Sinha. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
