8e427f3b902ba16e29f79fb0c817c2319664b858
Shaurya Sinha <ShauryaSinha@ASHUTOSHs-MacBook-Pro.local>
2018-01-17 18:39:47 -0500

    Lab 2 files

--- Tumblr-Feed/PhotosViewController.swift
+++ Tumblr-Feed/PhotosViewController.swift
@@ -92,14 +92,15 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        let vs = segue.destination as! PhotoDetailsViewController
     }
-    */
+    
 
 }
